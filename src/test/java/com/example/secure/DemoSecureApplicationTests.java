738f5fd1a53d1e614bddb12341fb1bdabaee9732
up1 <somkiat.p@gmail.com>
2025-11-16 21:36:35 +0700

    Initial project

---
+++ src/test/java/com/example/secure/DemoSecureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.secure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSecureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
